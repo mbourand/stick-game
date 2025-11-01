7a8d025dec51c37e6ec39e8dfa7b2945f218f4eb
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-01 22:11:02 +0100

    feat(env): :sparkles: add env support in backend

---
+++ apps/back/src/config/server.config.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const SERVER_CONFIG_SCHEMA = z.object({
+  PORT: z.coerce.number().min(1).max(65535),
+});
+
+export const SERVER_CONFIG: ServerConfigType = SERVER_CONFIG_SCHEMA.parse(process.env);
+
+export type ServerConfigType = z.infer<typeof SERVER_CONFIG_SCHEMA>;

9cbd08bd67bb1749319fbb6e98df00e8f9607acd
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-02 00:49:51 +0100

    feat(env): :sparkles: add env & backend types in front

--- apps/front/eslint.config.mjs
+++ apps/front/eslint.config.mjs
@@ -1,21 +1,24 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
-import { fixupConfigRules } from '@eslint/compat';
-import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import js from "@eslint/js";
+import { fixupConfigRules } from "@eslint/compat";
+import nx from "@nx/eslint-plugin";
+import baseConfig from "../../eslint.config.mjs";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
 });
 
 export default [
-  ...fixupConfigRules(compat.extends('next')),
-  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+  ...fixupConfigRules(compat.extends("next")),
+  ...fixupConfigRules(compat.extends("next/core-web-vitals")),
   ...baseConfig,
-  ...nx.configs['flat/react-typescript'],
+  ...pluginQuery.configs["flat/recommended"],
+  ...nx.configs["flat/react-typescript"],
   {
-    ignores: ['.next/**/*', '**/out-tsc'],
+    ignores: [".next/**/*", "**/out-tsc"],
   },
 ];
---
+++ apps/front/src/modules/env/Env.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  NEXT_PUBLIC_BACKEND_URL: z.url(),
+});
+
+export const Env = EnvSchema.parse(process.env);

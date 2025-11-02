2cebf56af44da5f4817680a0ef0ea3c96c7b678a
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-02 01:54:23 +0100

    feat(map-selection): :sparkles: add basic search beatmap feature

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./global.css";
+import { QueryProvider } from "@/components/QueryProvider";
 
 export const metadata = {
   title: "Welcome to Tau",
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
--- apps/front/src/modules/env/Env.ts
+++ apps/front/src/modules/env/Env.ts
@@ -4,4 +4,6 @@ const EnvSchema = z.object({
   NEXT_PUBLIC_BACKEND_URL: z.url(),
 });
 
-export const Env = EnvSchema.parse(process.env);
+export const Env = EnvSchema.parse({
+  NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+});

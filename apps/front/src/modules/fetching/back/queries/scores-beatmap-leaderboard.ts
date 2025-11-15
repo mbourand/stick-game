fe31c691210f76736955b59132942203bbf31cb7
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-15 14:48:19 +0100

    refactor(fetching): :recycle: remove options params from query options factories

--- apps/front/src/modules/fetching/back/queries/scores-beatmap-leaderboard.ts
+++ apps/front/src/modules/fetching/back/queries/scores-beatmap-leaderboard.ts
@@ -1,16 +1,12 @@
 import { fetchBackend } from "@/modules/fetching/back/fetchBackend";
-import { queryOptions, UseQueryOptions } from "@tanstack/react-query";
+import { queryOptions } from "@tanstack/react-query";
 
 export const fetchScoresBeatmapLeaderboard = (beatmapId: string) =>
   fetchBackend("/scores/:beatmapId/leaderboard", { params: { beatmapId } });
 
-export const createScoresBeatmapLeaderboardQueryOptions = (
-  beatmapId: string,
-  options?: Omit<UseQueryOptions<Awaited<ReturnType<typeof fetchScoresBeatmapLeaderboard>>>, "queryKey" | "queryFn">,
-) =>
+export const scoresBeatmapLeaderboardQueryOptions = (beatmapId: string) =>
   queryOptions({
     queryKey: ["scores", beatmapId, "leaderboard"],
     queryFn: () => fetchScoresBeatmapLeaderboard(beatmapId),
     staleTime: 1000 * 60 * 15,
-    ...options,
   });

0ca34251f14cd2fb8fbd586f2927f0c79586a76f
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-16 12:03:38 +0100

    feat(scores): :sparkles: add leaderboard component & prepare ui for local leaderboards

---
+++ apps/front/src/app/game/_components/MapLeaderboard/LeaderboardGlobalScores.tsx
@@ -0,0 +1,34 @@
+import { scoresBeatmapLeaderboardQueryOptions } from "@/modules/fetching/back/queries/scores-beatmap-leaderboard";
+import { useQuery } from "@tanstack/react-query";
+
+type LeaderboardGlobalScoresProps = {
+  beatmapId: string;
+};
+
+export const LeaderboardGlobalScores = ({ beatmapId }: LeaderboardGlobalScoresProps) => {
+  const leaderboardQuery = useQuery(scoresBeatmapLeaderboardQueryOptions(beatmapId ?? ""));
+
+  return (
+    <>
+      {leaderboardQuery.isLoading && <p>Loading leaderboard...</p>}
+      {leaderboardQuery.isError && <p>Error loading leaderboard</p>}
+      {leaderboardQuery.data && leaderboardQuery.data.leaderboard.length === 0 && <p>No scores yet</p>}
+      {leaderboardQuery.data && leaderboardQuery.data.leaderboard.length > 0 && (
+        <div className="flex flex-col gap-4 max-h-[500px] overflow-y-auto">
+          {leaderboardQuery.data.leaderboard.map((entry, index) => (
+            <li key={entry.beatmapId + "_" + entry.playerName}>
+              {index + 1}. {entry.playerName} -{" "}
+              {entry.score
+                .toString()
+                .split("")
+                .flatMap((c, i, a) => (i && (a.length - i) % 3 === 0 ? [" ", c] : [c]))
+                .join("")}
+              <br />
+              {entry.accuracy.toFixed(2)}% - {entry.maxCombo}x
+            </li>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
--- apps/front/src/modules/game/scenes/BeatmapSelection/BeatmapSelectionView.tsx
+++ apps/front/src/modules/game/scenes/BeatmapSelection/BeatmapSelectionView.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { type ParsedMap } from "../../../osu/convert/OsuConverter";
 import { MapPicker } from "@/app/game/_components/MapPicker";
-import { scoresBeatmapLeaderboardQueryOptions } from "@/modules/fetching/back/queries/scores-beatmap-leaderboard";
-import { useQuery } from "@tanstack/react-query";
+import { MapLeaderboard } from "@/app/game/_components/MapLeaderboard/MapLeaderboard";
 
 type BeatmapSelectionViewProps = {
   onPlayClicked: (selectedMap: ParsedMap) => void;
@@ -10,11 +9,6 @@ type BeatmapSelectionViewProps = {
 
 export const BeatmapSelectionView = ({ onPlayClicked }: BeatmapSelectionViewProps) => {
   const [selectedMap, setSelectedMap] = useState<ParsedMap | null>(null);
-
-  const leaderboardQuery = useQuery({
-    ...scoresBeatmapLeaderboardQueryOptions(selectedMap?.id ?? ""),
-    enabled: !!selectedMap,
-  });
 
   return (
     <>
@@ -29,29 +23,8 @@ export const BeatmapSelectionView = ({ onPlayClicked }: BeatmapSelectionViewProp
       >
         Play
       </button>
-      {leaderboardQuery.isEnabled && (
-        <div className="absolute left-0 top-1/2 -translate-y-1/2 text-white bg-white/10 p-6">
-          {leaderboardQuery.isLoading && <p>Loading leaderboard...</p>}
-          {leaderboardQuery.isError && <p>Error loading leaderboard</p>}
-
-          {leaderboardQuery.data && leaderboardQuery.data.leaderboard.length > 0 ? (
-            <div className="flex flex-col gap-4 max-h-[500px] overflow-y-auto">
-              {leaderboardQuery.data.leaderboard.map((entry, index) => (
-                <li key={entry.beatmapId + "_" + entry.playerName}>
-                  {index + 1}. {entry.playerName} -{" "}
-                  {entry.score
-                    .toString()
-                    .split("")
-                    .flatMap((c, i, a) => (i && (a.length - i) % 3 === 0 ? [" ", c] : [c]))
-                    .join("")}
-                  <br /> {entry.accuracy.toFixed(2)}% - {entry.maxCombo}x
-                </li>
-              ))}
-            </div>
-          ) : (
-            <p>No scores yet</p>
-          )}
-        </div>
+      {selectedMap?.id && (
+        <MapLeaderboard className="absolute left-0 top-1/2 -translate-y-1/2" beatmapId={selectedMap?.id} />
       )}
     </>
   );

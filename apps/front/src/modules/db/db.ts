203ef5a783c12b8fe249ff621999347c65eee308
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-17 22:17:41 +0100

    feat(scores): :sparkles: version all scores & discard old scores in server response

--- apps/front/src/modules/db/db.ts
+++ apps/front/src/modules/db/db.ts
@@ -1,7 +1,9 @@
 import { v1DexieDatabase } from "@/modules/db/versions/v1";
 import { v2DexieDatabase } from "@/modules/db/versions/v2";
+import { v3DexieDatabase } from "@/modules/db/versions/v3";
 
 export { latestDb } from "./versions";
 
 v1DexieDatabase();
 v2DexieDatabase();
+v3DexieDatabase();
--- apps/front/src/modules/db/queries/local-scores-beatmap-leaderboard.ts
+++ apps/front/src/modules/db/queries/local-scores-beatmap-leaderboard.ts
@@ -5,7 +5,11 @@ export const localScoresBeatmapLeaderboardQueryOptions = (beatmapId: string) =>
   queryOptions({
     queryKey: ["local-db", "local-scores", beatmapId],
     queryFn: async () => {
-      const scores = await latestDb.localScores.where("beatmapIdv2").equals(beatmapId).sortBy("score");
+      const scores = await latestDb.localScores
+        .where("beatmapIdv2")
+        .equals(beatmapId)
+        .and((score) => score.scoreVersion === 2)
+        .sortBy("score");
       return scores.reverse();
     },
   });

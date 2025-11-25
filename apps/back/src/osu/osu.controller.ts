6e91f420eaa839089b2cebbf3afd5a1f140fb231
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-11-25 11:29:18 +0100

    feat(map-selection): :sparkles: make beatmap search route public

--- apps/back/src/osu/osu.controller.ts
+++ apps/back/src/osu/osu.controller.ts
@@ -5,12 +5,14 @@ import {
   GetOsuBeatmapsetsSearchResponseDto,
 } from "./dto/routes/get-osu-beatmapsets-search.dto";
 import { ZodResponse } from "nestjs-zod";
+import { Public } from "../auth/public.decorator";
 
 @Controller("osu")
 export class OsuController {
   constructor(private readonly osuService: OsuService) {}
 
   @Get("beatmapsets/search")
+  @Public()
   @ZodResponse({ type: GetOsuBeatmapsetsSearchResponseDto })
   async beatmapsetsSearch(@Query() query: GetOsuBeatmapsetsSearchQueryParamsDto) {
     return this.osuService.searchBeatmapsets(query.q);
